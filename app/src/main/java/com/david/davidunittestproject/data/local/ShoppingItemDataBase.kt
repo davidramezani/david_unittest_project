433324e13a13e6877f4bab32d6fa3b78c3d101df
David Ramezani <dav.ramezani@gmail.com>
2021-08-11 20:45:31 +0430

    add shopping item model and entity and dao

---
+++ app/src/main/java/com/david/davidunittestproject/data/local/ShoppingItemDataBase.kt
@@ -0,0 +1,14 @@
+package com.david.davidunittestproject.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [ShoppingItem::class],
+    version = 1
+)
+abstract class ShoppingItemDataBase : RoomDatabase() {
+
+    abstract fun shoppingDao() : ShoppingDao
+
+}
